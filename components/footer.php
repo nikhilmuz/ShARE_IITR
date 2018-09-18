c0435a911d0590d44a965a857df476f938acc347
Nikhil Kumar <nikhil.nikhil.muz@gmail.com>
2018-09-18 05:44:37 +0530

    CSS Updates

--- components/footer.php
+++ components/footer.php
@@ -1,25 +1,25 @@
 <div class="footer">
         <div style="background-color: #0aca86;" class="row">
         <div class="col-md-4">
-            <p>About</p>
+            <p class="footer-heading">About</p>
             <ul class="list">
             <li>Prama Jyoti Foundation (PJF)</li>
             <li>ShARE - IITR</li>
             </ul>
         </div>
         <div class="col-md-4">
-            <p>Contact</p>
+            <p class="footer-heading">Contact</p>
             <p>+91 - 9345678987</p>
             <p>share@iitr.ac.in</p>
         </div>
         <div class="col-md-4">
-            <p>Find Us</p>
+            <p class="footer-heading">Find Us</p>
             <p></p>
         </div>
         </div>
         <div style="background-color: #08af74;" class="row">
         <div style="height: 35px;" class="col-md-12">
-            <p align="center">Designed by: <img height="30px" src="assets/images/ds_logo.png"></p>
+            <p class="footer-copy" align="center">Designed by:&nbsp;&nbsp;&nbsp;&nbsp;<img height="30px" src="assets/images/ds_logo.png"></p>
         </div>
          </div>
             </div>
--- index.php
+++ index.php
@@ -8,24 +8,23 @@
 		<div class="col-md-6">
 			<img class="bg1" src="assets/images/bg1.png">
 		</div>
-
 	</div>
 <div class="row content">
 	<div class="heading col-md-6">
-		<p>GLOBAL STRATEGY</p>
-		<p>CASE CHALLENGE</p>
+		<p class="topic">GLOBAL STRATEGY</p>
+		<p class="topic">CASE CHALLENGE</p>
 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-		<p>14 December - 20 December</p>
+		<p class="deadline">14 December - 20 December</p>
         <p><input type="button" value="Register Now" class="btn solid-btn"></p>
-		<p>Hurry Up! Registration closes 2 November</p>
+		<p class="info">Hurry Up! Registration closes 2 November</p>
 	</div>
 </div>
-<div class="row info">
+<div class="row padding-10">
 	<div class="col-md-12">
-		<p align="center">What is global strategy case challenge?</p>
-		<p class="info-disc" align="center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam mattis augue sit amet tempor auctor. Donec eu odio sed nulla laoreet tristique. Cras at egestas justo. Fusce in lobortis augue. Phasellus sapien eros, lobortis ac ligula id, semper placerat nulla. Donec viverra odio vel augue elementum sodales</p>
+		<p class="head-text" align="center">What is global strategy case challenge?</p>
+		<p class="padding-10" align="center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam mattis augue sit amet tempor auctor. Donec eu odio sed nulla laoreet tristique. Cras at egestas justo. Fusce in lobortis augue. Phasellus sapien eros, lobortis ac ligula id, semper placerat nulla. Donec viverra odio vel augue elementum sodales</p>
 		<p align="center"><input type="button" value="Know More" class="btn empty-btn"></p>
-        <p class="info-disc" align="center">Exciting opportunities to grab!</p>
+        <p class="head-text padding-10" align="center">Exciting opportunities to grab!</p>
     </div>
     <div class="col-md-4">
         <p align="center"><img width="30%" src="assets/images/icon1.png"></p>
@@ -33,7 +32,7 @@
     </div>
     <div class="col-md-4">
         <p align="center"><img width="30%" src="assets/images/icon1.png"></p>
-        <p align="center">Cash Prizes worth Rs 1,00,000</p>
+        <p align="center">Opportunity to present in front of renowned researchers</p>
     </div>
     <div class="col-md-4">
         <p align="center"><img width="30%" src="assets/images/icon1.png"></p>
