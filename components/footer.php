db1d26c7250624de2f1fec13d4c86097ff292f3c
Nikhil Kumar <nikhil.nikhil.muz@gmail.com>
2018-09-19 14:50:44 +0530

    Footer Links

--- components/footer.php
+++ components/footer.php
@@ -3,25 +3,25 @@
         <div class="col-md-4">
             <p class="footer-heading">About</p>
             <ul class="list">
-            <li>Prama Jyoti Foundation (PJF)</li>
+                <li><a href="http://www.pramajyoti.org/">Prama Jyoti Foundation (PJF)</a></li>
             <li>ShARE - IITR</li>
             </ul>
         </div>
         <div class="col-md-4">
             <p class="footer-heading">Contact</p>
             <ul class="list">
-                <li>+91 - 9345678987</li>
-                <li>share@iitr.ac.in</li>
+                <li><a href="tel:+919345678987">+91 - 9345678987</a></li>
+                <li><a href="mailto:share@iitr.ac.in">share@iitr.ac.in</a></li>
             </ul>
         </div>
         <div class="col-md-4">
             <p class="footer-heading">Find Us</p>
-            <p></p>
+            <p><a href="https://www.facebook.com/shareiitr/"><i class="fab fa-facebook-f"></i></a> <a href="#"><i class="fab fa-linkedin-in"></i></a> <a href="https://twitter.com/shareiitr"><i class="fab fa-twitter"></i></a></p>
         </div>
         </div>
         <div style="background-color: #08af74;" class="row">
         <div style="height: 35px;" class="col-md-12">
-            <p class="footer-copy" align="center">Designed by:&nbsp;&nbsp;&nbsp;&nbsp;<img height="30px" src="assets/images/ds_logo.png"></p>
+            <p class="footer-copy" align="center">Designed by:&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://designstudio.cc/"><img height="30px" src="assets/images/ds_logo.png"></a></p>
         </div>
          </div>
             </div>
