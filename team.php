b2f895d6017a307191a5447fd7527f12dcfe1c9b
Nikhil Kumar <nikhil.nikhil.muz@gmail.com>
2018-10-07 18:07:17 +0530

    Member hover social

--- team.php
+++ team.php
@@ -17,47 +17,96 @@
         <p class="head-text">Members</p>
         <div class="row">
             <div class="col-md-4">
-                <img src="assets/images/avatar.png" width="80%">
-                <div class="name">John Doe</div>
-                <div class="intro">lorem iosum dolor</div>
+                <div class="member">
+                    <div class="avatar">
+                        <img src="assets/images/avatar.png">
+                        <div class="member-mask">
+                            <p class="member-social"><a href="#"><i class="fab fa-facebook-f"></i></a>&nbsp;&nbsp;&nbsp;<a href="#"><i class="fab fa-linkedin-in"></i></a></p>
+                        </div>
+                    </div>
+                    <div class="name">John Doe</div>
+                    <div class="intro">lorem iosum dolor</div>
+                </div>
             </div>
             <div class="col-md-8">
                 <p align="justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin euismod non leo et sollicitudin. Aliquam at auctor ante. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tincidunt viverra nisi, eu molestie neque accumsan sit amet. Mauris tristique risus lacinia velit vulputate, sit amet euismod erat tincidunt. Ut interdum, felis vitae molestie lacinia, turpis libero pulvinar nisi, ut faucibus leo sem at purus. In sit amet nibh vel tellus tempus consequat a eget dolor. Donec id vestibulum nibh. Donec euismod elit a lacus ornare, eget feugiat mauris varius. Integer vulputate nisi euismod mauris egestas, vel cursus turpis congue. Fusce pretium, turpis sed lobortis lacinia, justo diam rhoncus erat, in malesuada justo leo in diam. Morbi quis sapien feugiat ligula pharetra hendrerit. Maecenas ante nisi, scelerisque a dui a, cursus convallis velit. Nulla quis ligula nunc. Nullam arcu lorem, facilisis sed dictum sit amet, bibendum in orci. Mauris ornare cursus ex eu feugiat.</p>
             </div>
         </div>
         <div class="row">
-    <div class="col-md-4">
-        <img src="assets/images/avatar.png" width="80%">
-        <div class="name">John Doe</div>
-        <div class="intro">lorem iosum dolor</div>
-    </div>
-    <div class="col-md-4">
-        <img src="assets/images/avatar.png" width="80%">
-        <div class="name">John Doe</div>
-        <div class="intro">lorem iosum dolor</div>
-    </div>
-    <div class="col-md-4">
-        <img src="assets/images/avatar.png" width="80%">
-        <div class="name">John Doe</div>
-        <div class="intro">lorem iosum dolor</div>
-    </div>
+            <div class="col-md-4">
+                <div class="member">
+                    <div class="avatar">
+                        <img src="assets/images/avatar.png">
+                        <div class="member-mask">
+                            <p class="member-social"><a href="#"><i class="fab fa-facebook-f"></i></a>&nbsp;&nbsp;&nbsp;<a href="#"><i class="fab fa-linkedin-in"></i></a></p>
+                        </div>
+                    </div>
+                    <div class="name">John Doe</div>
+                    <div class="intro">lorem iosum dolor</div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="member">
+                    <div class="avatar">
+                        <img src="assets/images/avatar.png">
+                        <div class="member-mask">
+                            <p class="member-social"><a href="#"><i class="fab fa-facebook-f"></i></a>&nbsp;&nbsp;&nbsp;<a href="#"><i class="fab fa-linkedin-in"></i></a></p>
+                        </div>
+                    </div>
+                    <div class="name">John Doe</div>
+                    <div class="intro">lorem iosum dolor</div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="member">
+                    <div class="avatar">
+                        <img src="assets/images/avatar.png">
+                        <div class="member-mask">
+                            <p class="member-social"><a href="#"><i class="fab fa-facebook-f"></i></a>&nbsp;&nbsp;&nbsp;<a href="#"><i class="fab fa-linkedin-in"></i></a></p>
+                        </div>
+                    </div>
+                    <div class="name">John Doe</div>
+                    <div class="intro">lorem iosum dolor</div>
+                </div>
+            </div>
         </div>
         <div class="row">
-    <div class="col-md-4">
-        <img src="assets/images/avatar.png" width="80%">
-        <div class="name">John Doe</div>
-        <div class="intro">lorem iosum dolor</div>
-    </div>
-    <div class="col-md-4">
-        <img src="assets/images/avatar.png" width="80%">
-        <div class="name">John Doe</div>
-        <div class="intro">lorem iosum dolor</div>
-    </div>
-    <div class="col-md-4">
-        <img src="assets/images/avatar.png" width="80%">
-        <div class="name">John Doe</div>
-        <div class="intro">lorem iosum dolor</div>
-    </div>
+            <div class="col-md-4">
+                <div class="member">
+                    <div class="avatar">
+                        <img src="assets/images/avatar.png">
+                        <div class="member-mask">
+                            <p class="member-social"><a href="#"><i class="fab fa-facebook-f"></i></a>&nbsp;&nbsp;&nbsp;<a href="#"><i class="fab fa-linkedin-in"></i></a></p>
+                        </div>
+                    </div>
+                    <div class="name">John Doe</div>
+                    <div class="intro">lorem iosum dolor</div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="member">
+                    <div class="avatar">
+                        <img src="assets/images/avatar.png">
+                        <div class="member-mask">
+                            <p class="member-social"><a href="#"><i class="fab fa-facebook-f"></i></a>&nbsp;&nbsp;&nbsp;<a href="#"><i class="fab fa-linkedin-in"></i></a></p>
+                        </div>
+                    </div>
+                    <div class="name">John Doe</div>
+                    <div class="intro">lorem iosum dolor</div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="member">
+                    <div class="avatar">
+                        <img src="assets/images/avatar.png">
+                        <div class="member-mask">
+                            <p class="member-social"><a href="#"><i class="fab fa-facebook-f"></i></a>&nbsp;&nbsp;&nbsp;<a href="#"><i class="fab fa-linkedin-in"></i></a></p>
+                        </div>
+                    </div>
+                    <div class="name">John Doe</div>
+                    <div class="intro">lorem iosum dolor</div>
+                </div>
+            </div>
         </div>
         <br/>
         <p class="head-text">Prama Jyoti Foundation</p>
